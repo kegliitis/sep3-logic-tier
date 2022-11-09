8ecb90fe6a903282121ae1a9dedcfafc1628977d
apeTrackunit <ape@trackunit.com>
2022-11-09 15:14:38 +0100

    Fixed port and renamed webapi to controllers

--- src/main/java/via/sep3/controller/ReportsController.java
+++ src/main/java/via/sep3/controller/ReportsController.java
@@ -1,4 +1,4 @@
-package via.sep3.webapi;
+package via.sep3.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import via.sep3.grpcclient.GrpcClient;
 import via.sep3.grpcclient.GrpcClientImpl;
+import via.sep3.model.Location;
 import via.sep3.model.Report;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,10 +23,10 @@ public class ReportsController
     public String getReports()
     {
         List<Report> reports = new ArrayList<>();
-        //reports.add(new Report(LocalDate.now(), LocalTime.now(), null, "it is a report", "Under Review", new Location(1.1, 2.2, (byte) 1)));
+        reports.add(new Report(LocalDate.now(), LocalTime.now(), null, "it is a report", "Under Review", new Location(1.1, 2.2, (byte) 1)));
 
-        GrpcClient grpcClient = new GrpcClientImpl();
-        reports = grpcClient.getReports();
+        //GrpcClient grpcClient = new GrpcClientImpl();
+        //reports = grpcClient.getReports();
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
