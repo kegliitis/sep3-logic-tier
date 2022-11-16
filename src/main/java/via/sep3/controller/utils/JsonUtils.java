fbb0d6eb8b3449cff0b23a5e83ce3f9941a1a2bd
Adam Petricek <78472079+apeTrackunit@users.noreply.github.com>
2022-11-16 12:59:43 +0100

    View reports (#2)
    * test commit by andras
    
    * Fixed port and renamed webapi to controllers
    
    * New readme
    
    * More data
    
    * CORS fix
    
    * Working POC
    
    * JSON utils
    
    * remove biscuits
    
    * Exception fix
    
    Co-authored-by: András Kovács <k.andras9@gmail.com>

---
+++ src/main/java/via/sep3/controller/utils/JsonUtils.java
@@ -0,0 +1,24 @@
+package via.sep3.controller.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JsonUtils {
+    public static String convertToJson(Object input) throws Exception{
+        try
+        {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            mapper.registerModule(new JavaTimeModule());
+
+            String reportsAsJson;
+            reportsAsJson = mapper.writeValueAsString(input);
+
+            return reportsAsJson;
+        } catch (JsonProcessingException e) {
+            throw new Exception(e);
+        }
+    }
+}
