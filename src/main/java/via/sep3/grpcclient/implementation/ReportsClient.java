4e5c9fc540655b6edc9c7f5c582a6f9f2d0cc255
James Cullen <jcullen.dk@gmail.com>
2022-11-30 13:08:04 +0100

    >>Report model - id field added, - changed id to string which needs to be changed in DB >>CreateReport model created - fields with getters and setters >>ReportsController - changed to call repository, not gRPCclient - creates from "/reports" >>Report proto added CreateReportObject - rpc CreateReport(CreateReportObject) returns (ReportObject) - sets date,time,proof,description,status,location - returns new Report with id and all the above

--- src/main/java/via/sep3/grpcclient/implementation/ReportsClient.java
+++ src/main/java/via/sep3/grpcclient/implementation/ReportsClient.java
@@ -4,6 +4,7 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.springframework.stereotype.Service;
 import via.sep3.grpcclient.client.IReportsClient;
+import via.sep3.model.CreateReport;
 import via.sep3.model.Location;
 import via.sep3.model.Report;
 import via.sep3.protobuf.report.ReportFilter;
@@ -25,6 +26,23 @@ public class ReportsClient implements IReportsClient
             .build();
 
     private ReportGrpc.ReportBlockingStub reportBlockingStub = ReportGrpc.newBlockingStub(managedChannel);
+
+
+    @Override
+    public Report createReport(CreateReport newReport) {
+        CreateReportInput input = CreateReportInput.newBuilder()
+                .setDate(newReport.getDate())
+                .setTime(newReport.getTime())
+                .setProof(newReport.getProof())
+                .setDescription(newReport.getDescription())
+                .setStatus(newReport.getStatus());
+
+        ReportObject response = reportBlockingStub.createReport(input);
+
+        return new Report(/*response.getId(),*/(LocalDate.parse(response.getDate()), (LocalTime.parse(response.getTime())),response.getProof(),
+                response.getDescription(), response.getStatus(),
+                new Location(response.getLocation().getLatitude(),response.getLocation().getLongitude(),(byte)response.getLocation().getSize()));
+    }
 
     @Override
     public List<Report> getReports()
